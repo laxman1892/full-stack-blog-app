addd8a461e8216c995d2020e8e84afe0725fd15b
laxman1892 <lxmnrzl@gmail.com>
2024-04-23 22:47:42 +0545

    incomplete fetching from post route

--- client/src/components/pages/CreatePost.js
+++ client/src/components/pages/CreatePost.js
@@ -1,42 +1,74 @@
-import { useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';  
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 const modules = {
-        toolbar: [
-            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-            ['blockquote', 'code-block'],
-            ['link', 'image', 'video', 'formula'],
-          
-            [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-            [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-            [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-            [{ 'direction': 'rtl' }],                         // text direction
-          
-            [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-          
-            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-            [{ 'font': [] }],
-            [{ 'align': [] }],
-          
-            ['clean']                                         // remove formatting button
-          ]
-    };
+  toolbar: [
+    ["bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote", "code-block"],
+    ["link", "image", "video", "formula"],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
+
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ["clean"], // remove formatting button
+  ],
+};
 
 export default function CreatePost() {
-    const [title, setTitle] = useState('');
-    const [summary, setSummary] = useState('');
-    const  [content, setContent] = useState('');
-    
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
 
-    return (
-        <form className='form' style={{height: '100px'}}>
-            <input type="title"  placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} />
-            <input type="summary"  placeholder="Summary" value={summary} onChange={e => setSummary(e.target.value)} />
-            <input type="file" />
-            <ReactQuill value={content} modules={modules} onChange={newValue => setContent(newValue)} style={{overflowY: 'auto'}} />
-            <button style={{marginTop: 5}}>Create Post</button> 
-        </form>
-    )
+  async function createNewPost(e) {
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('file', files[0]);
+
+    e.preventDefault();
+    const response = await fetch("https://localhost:4000/post", {
+      method: "POST",
+      body: data,
+    });
+  }
+
+  return (
+    <form className="form" style={{ height: "100px" }} onSubmit={createNewPost}>
+      <input
+        type="title"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder="Summary"
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
+      <input
+        type="file"
+        onChange={(e) => setFiles(e.target.files)}
+      />
+      <ReactQuill
+        value={content}
+        modules={modules}
+        onChange={(newValue) => setContent(newValue)}
+        style={{ overflowY: "auto" }}
+      />
+      <button style={{ marginTop: 5 }}>Create Post</button>
+    </form>
+  );
 }
