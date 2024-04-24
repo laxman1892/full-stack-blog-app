fd2dfa3d8e86d105779ec26f8c88fbd3951011c6
laxman1892 <lxmnrzl@gmail.com>
2024-04-24 08:10:31 +0545

    incomplete update to get posts from server

--- client/src/components/pages/Page.js
+++ client/src/components/pages/Page.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Post from '../Post'
 
 export default function Page() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/post').then(response => {
+      response.json().then(posts => {
+        setPosts(posts)
+      });
+    });
+  }, [])
   return (
     <div>
-        <Post />
-        <Post />
-        <Post />
+        {posts.length > 0 && posts.map(post => (
+          <Post />
+        ))}
     </div>
   )
 }
